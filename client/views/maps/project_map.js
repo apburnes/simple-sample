0ce6b2dffd92aa8dadc483981c188726ff827fe8
Andrew Burnes <apburnes@gmail.com>
2013-10-17 23:14:13 -0700

    Testing underscore map boundary set

--- client/views/maps/project_map.js
+++ client/views/maps/project_map.js
@@ -1,12 +1,34 @@
 Template.projectMap.rendered = function () {
 	var markers;
-	Map = L.map('project-map').setView([33.4500, -112.0667], 10);
-	
+
+	var projectId = Session.get('currentProjectId')
+	var pointList = Geoms.find({projectId: projectId}).fetch();
+	var pointCount = Geoms.find({projectId: projectId}).count();
+
+	Map = L.map('project-map').setView([33.4500, -112.0667], 8);
+
+	var latList = [];
+	var lngList = [];
+
+	if (pointCount > 0) {
+		_.each(pointList, function (geo) {
+			latList.push([geo.lat]);
+			lngList.push([geo.lng]);
+		});
+
+		var minLat = _.min(latList)[0];
+		var maxLat = _.max(latList)[0];
+		var minLng = _.min(lngList)[0];
+		var maxLng = _.max(lngList[0]);
+
+		var bndry = [[minLat, minLng],[maxLat,maxLng]];
+	}
+
 	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
 		maxZoom: 18
 	}).addTo(Map);
-	
+
 	markers = new L.LayerGroup().addTo(Map)
 
 	Deps.autorun(function () {
@@ -16,7 +38,7 @@ Template.projectMap.rendered = function () {
 		var publicIcon = L.icon({
 			iconUrl: "/public-marker.png" 
 		});
-		
+
 		markers.clearLayers();
 
 		_.each(pointList, function (geo) {
